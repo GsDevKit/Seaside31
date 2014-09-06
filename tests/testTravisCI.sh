be49804f7ad3d85f3a077e2ea91cfe4d178cf353
Johan Brichau <johan@inceptive.be>
2014-09-06 16:25:51 +0200

    updating GLASS1 load in travis test

--- tests/testTravisCI.sh
+++ tests/testTravisCI.sh
@@ -37,35 +37,30 @@ cat - >> $OUTPUT_PATH << EOF
 "Load and run tests to be performed by TravisCI"
 Transcript cr; show: 'travis---->travisCI.st'.
 
+"Upgrade GLASS to to 1.0-beta.9.3"
+Gofer new
+    url: 'http://seaside.gemtalksystems.com/ss/MetacelloRepository';
+    package: 'ConfigurationOfGLASS';
+    load.
+ConfigurationOfGLASS project updateProject.
+GsDeployer
+  deploy: [ (ConfigurationOfGLASS project version: '1.0-beta.9.3') load ].
+"Install GLASS from github"
 GsDeployer deploy: [
-  | glassVersion |
-  glassVersion := ConfigurationOfGLASS project currentVersion.
-  glassVersion versionNumber < '1.0-beta.9.3' asMetacelloVersionNumber
-    ifTrue: [
-      Transcript
-        cr;
-        show: '-----Upgrading GLASS to 1.0-beta.9.3'.
-      GsDeployer deploy: [
-        Gofer new
-          package: 'ConfigurationOfGLASS';
-          url: 'http://seaside.gemtalksystems.com/ss/MetacelloRepository';
-          load.
-        (((System stoneVersionAt: 'gsVersion') beginsWith: '2.') and: [glassVersion versionNumber < '1.0-beta.9.2' asMetacelloVersionNumber])
-          ifTrue: [
-            ((Smalltalk at: #ConfigurationOfGLASS) project version: '1.0-beta.9.2') load ].
-        ((Smalltalk at: #ConfigurationOfGLASS) project version: '1.0-beta.9.3') load.
-      ] ]
-    ifFalse: [
-      Transcript
-        cr;
-        show: '-----GLASS already upgraded to 1.0-beta.9.3' ] ].
-
-GsDeployer deploy: [
-  "Explicitly load latest Grease configuration, since we're loading the #bleeding edge"
-  Metacello new
-    configuration: 'Grease';
-    repository: 'http://www.smalltalkhub.com/mc/Seaside/MetacelloConfigurations/main';
-    get.
+ Metacello new
+  baseline: 'GLASS1';
+  repository: 'github://glassdb/glass:master/repository';
+  get.
+ Metacello new
+  baseline: 'GLASS1';
+  repository: 'github://glassdb/glass:master/repository';
+  onConflict: [ :ex | ex allow ];
+  onWarning: [ :ex | 
+        Transcript
+          cr;
+          show: ex description.
+        ex resume ];
+  load: 'default' ].
 
   "Load the configuration or baseline"
   Metacello new
